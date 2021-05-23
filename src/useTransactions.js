a7b5e8e565d7624acca478a0c4edacea2a31637d
Anku <70649392+jaswalsaurabh@users.noreply.github.com>
2021-05-23 21:19:55 +0530

    background image is changed

--- src/useTransactions.js
+++ src/useTransactions.js
@@ -18,7 +18,6 @@ const {transactions} = useContext(ExpenseTrackerContext);
 const transactionsPerType = transactions.filter((t)=> t.type === title);
 const total  = transactionsPerType.reduce((acc,currVal)=> acc+= currVal.amount,0);
 const categories = title === 'Income' ? incomeCategories : expenseCategories;
-// console.log(transactionsPerType,total,categories)
 
 transactionsPerType.forEach((t)=>{
     const category = categories.find((c)=> c.type === t.category)
