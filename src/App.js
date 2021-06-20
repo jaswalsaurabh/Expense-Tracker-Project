54f52ac1da1558d9ea7c6b81ddc5339bf47a2bf3
Anku <70649392+jaswalsaurabh@users.noreply.github.com>
2021-06-20 18:36:07 +0530

    code pushed

--- src/App.js
+++ src/App.js
@@ -1,95 +1,117 @@
-import React, { useEffect, useRef } from 'react'
-import Details from './components/Details/Details'
-import { Grid } from '@material-ui/core';
-import { PushToTalkButton, PushToTalkButtonContainer, ErrorPanel } from '@speechly/react-ui'
+import React, { useEffect, useRef } from "react";
+import Details from "./components/Details/Details";
+import { Grid } from "@material-ui/core";
+import {
+  PushToTalkButton,
+  PushToTalkButtonContainer,
+  ErrorPanel,
+} from "@speechly/react-ui";
 
-import { SpeechState, useSpeechContext } from '@speechly/react-client'
-import useStyles from './styles.js';
-import Main from './components/Main/Main';
-import ChatBot from 'react-simple-chatbot';
+import { SpeechState, useSpeechContext } from "@speechly/react-client";
+import useStyles from "./styles.js";
+import Main from "./components/Main/Main";
+import ChatBot from "react-simple-chatbot";
 
 const steps = [
-    {
-        id: '0',
-        message: 'Welcome to Expense Tracker!',
-        trigger: '1',
-    },
-    {
-        id: '1',
-        message: 'My name is Sandy',
-        trigger: '2',
-    },
-    {
-        id: '2',
-        message: 'Can you tell me your name!',
-        trigger: '3'
-    },
-    {
-        id: '3',
-        user: true,
-        trigger: '4'
-    },
-    {
-        id: '4',
-        message: 'Hi {previousValue}, nice to meet you!',
-        trigger: '5',
-    },
-    {
-        id: '5',
-        message: 'How I can help you?',
-        trigger: '6',
-    },
-    {
-        id: '6',
-        user: true,
-        trigger: '7'
-    },
-    {
-        id: '7',
-        message: 'Ok!',
-        end: true
-    },
+  {
+    id: "0",
+    message: "Welcome to Expense Tracker!",
+    trigger: "1",
+  },
+  {
+    id: "1",
+    message: "My name is Sandy",
+    trigger: "2",
+  },
+  {
+    id: "2",
+    message: "Can you tell me your name!",
+    trigger: "3",
+  },
+  {
+    id: "3",
+    user: true,
+    trigger: "4",
+  },
+  {
+    id: "4",
+    message: "Hi {previousValue}, nice to meet you!",
+    trigger: "5",
+  },
+  {
+    id: "5",
+    message: "How I can help you?",
+    trigger: "6",
+  },
+  {
+    id: "6",
+    user: true,
+    trigger: "7",
+  },
+  {
+    id: "7",
+    message: "Ok!",
+    trigger: "8",
+  },
+  {
+    id: "8",
+    message: "You can go on this link, https://bit.ly/3wKmbP0",
+    end: true,
+  },
 ];
 
 const App = () => {
-    const classes = useStyles();
-    const { speechState } = useSpeechContext()
-    const main = useRef(null);
+  const classes = useStyles();
+  const { speechState } = useSpeechContext();
+  const main = useRef(null);
 
-    const executeScroll = () => main.current.scrollIntoView();
+  const executeScroll = () => main.current.scrollIntoView();
 
-    useEffect(() => {
-        if (speechState === SpeechState.Recording) {
-            executeScroll();
-        }
-    }, [speechState])
-    return (
-        <>
-            <div>
-                <Grid className={classes.grid} container spacing={0} alignItems='center' justify='center' style={{ height: '100vh' }} >
-                    <Grid item xs={12} sm={4} className={classes.mobile} >
-                        <Details title='Income' />
-                    </Grid>
-                    <Grid ref={main} item xs={12} sm={3} className={classes.main} >
-                        <Main />
-                    </Grid>
-                    <Grid item xs={12} sm={4} className={classes.desktop} >
-                        <Details title='Income' />
-                    </Grid>
-                    <Grid item xs={12} sm={4} className={classes.last} >
-                        <Details title='Expense' />
-                    </Grid>
-                </Grid>
-                <PushToTalkButtonContainer>
-                    <PushToTalkButton />
-                    <ErrorPanel />
-                </PushToTalkButtonContainer>
-                <div style={{ position: 'absolute', right: 20, top: 300, }}>
-                    <ChatBot headerTitle='Your Support' customDelay={1000} floating speechSynthesis={{ enable: true, lang: 'en' }} steps={steps} />
-                </div>
-            </div>
-        </>
-    )
-}
+  useEffect(() => {
+    if (speechState === SpeechState.Recording) {
+      executeScroll();
+    }
+  }, [speechState]);
+  return (
+    <>
+      <div>
+        <Grid
+          className={classes.grid}
+          container
+          spacing={0}
+          alignItems="center"
+          justify="center"
+          style={{ height: "100vh" }}
+        >
+          <Grid item xs={12} sm={4} className={classes.mobile}>
+            <Details title="Income" />
+          </Grid>
+          <Grid ref={main} item xs={12} sm={3} className={classes.main}>
+            <Main />
+          </Grid>
+          <Grid item xs={12} sm={4} className={classes.desktop}>
+            <Details title="Income" />
+          </Grid>
+          <Grid item xs={12} sm={4} className={classes.last}>
+            <Details title="Expense" />
+          </Grid>
+        </Grid>
+        <PushToTalkButtonContainer>
+          <PushToTalkButton />
+          <ErrorPanel />
+        </PushToTalkButtonContainer>
+        <div style={{ position: "absolute", right: 20, top: 300 }}>
+          <ChatBot
+            headerTitle="Your Support"
+            customDelay={1000}
+            floating
+            speechSynthesis={{ enable: true, lang: "en" }}
+            steps={steps}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -7,8 +7,8 @@ const InfoCard = () => {
         <div style={{textAlign: 'center', padding: '0 10%'}} >
             Try saying: <br/>
             Add {isIncome ? 'Income ' : 'Expense '}
-            for {isIncome ? '₹100 ' : '₹50 '}
             in Category {isIncome ? 'Business ' : 'House '}   
+            of {isIncome ? '₹100 ' : '₹50 '}
             for {isIncome ? 'Monday ' : 'Tuesday '}
         </div>
     )
